5b0daa338589a7ce545083b65fce8d902cdcfcab
dpappadopulo <duccio.pappadopulo@gmail.com>
2019-08-03 10:27:13 -0400

    fix cython, add run file

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
+import numpy
 
-setup(ext_modules = cythonize('data_producer.pyx'))
+setup(ext_modules = cythonize('data_producer.pyx'), include_dirs=[numpy.get_include()])
